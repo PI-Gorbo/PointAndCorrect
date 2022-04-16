d1354d00664e4f684d8ec70db0db89fa960f2ad6
Sam <sam.i.gorbwork@gmail.com>
2022-04-16 12:53:13 +1000

    Added to addListeners.js and manifest.json to add keyboard shortcut functionality. AddListeners.js will call a content script suggestion.js

--- src/background/addListeners.js
+++ src/background/addListeners.js
@@ -1,3 +1,9 @@
-html {
-    font-family: "Arial", "Times New Roman";
-}
+// (A) Adds an event listener to the browser. It listens for the command (defiend in manifest.json)
+// and, if it is the correct command, triggers the correction code.
+browser.commands.onCommand.addListener(function (command) {
+    if (command === "trigger-correction") {
+        console.log("I was triggered by the keyboard shortcut!")
+    }
+});
+
+browser.tabs.query({currentWindow: true}, (input) => console.log("TRIGGERED WOOHOO"));
