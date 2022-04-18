8f9c0ea1ba42aee9717747f2c364c91f93a4c0c9
Sam <sam.i.gorbwork@gmail.com>
2022-04-19 00:32:38 +1000

    Added a temporary file and added to suggestion.js/css/html to make a box with 4 buttons.

--- src/content/suggestion.js
+++ src/content/suggestion.js
@@ -4,32 +4,41 @@
 // (A) Make sure that there is no current suggestion from this script
 
 // (B) If not, create a suggestion box at the top left corner of the selection and wait for a button click
+
+
 //  (B1) Get the bounding box of the selection
 let selection = null;
 selection = window.getSelection();
 if (selection === null || selection.rangeCount === 0 || selection.toString().length === 0) { //If there is nothing in the selection, do nothing
     throw "suggestion.js error with getting selection : No selection by the user";
 }
-console.log("created selection : ", selection.toString());
+console.log("Selection by user: ", selection.toString());
 
 let rangeOfSelection = selection.getRangeAt(0); //Gets the first range 
 let rect = rangeOfSelection.getBoundingClientRect(); // Creates a rectangle that outlines the highlighted text
 console.log(rect);
 
+function createBox(top, left, width, height, parent) {
+    let box = document.createElement("iframe");
+    box.src = "src/content/suggestion.html"
+    box.style.top = (top + window.scrollY) + "px";
+    box.style.left = (left + window.scrollX) + "px";
+    box.style.position = "absolute";
+    parent.appendChild(box);
+    return box;
+}
 //  (B2) At the top left corner of the bounding box, create an iframe that will link to suggestion.html
-
-let suggestionBox = document.createElement("div");
-// suggestionBox.src = "src/content/suggestion.html";
-suggestionBox.title = "SuggestionBox";
-suggestionBox.style.top = "200px";
-suggestionBox.style.right = "0px";
-suggestionBox.style.width = rect.width.toString() + "px";
-suggestionBox.style.height = rect.height.toString() + "px";
-suggestionBox.style.backgroundColor = "black";
-document.body.appendChild(suggestionBox);
-console.log(suggestionBox);
+try {
+    let suggestionBox = createBox(rect.bottom, rect.left, rect.width, rect.height, document.body);
+}
+catch (err) {
+    console.log("Failed to generate box", err);
+    throw("suggestion.js failed to generate box.");
+}
+console.log("box that was created", suggestionBox);
 
 // // (C) React to clicks 
 
 // // (D) Return and finish normal functioning.
 undefined;
+
